d1d8422963fb81cf2692c9a1d8c2359ccb372d59
mouden-idd17 <moudenhoven@berkeley.edu>
2017-11-04 12:28:10 -0700

    bluetooth integration mostly completed, background image padding issue with interface still present

---
+++ picoproapp/src/main/java/com/example/androidthings/myproject/HomeActivity.java
@@ -0,0 +1,307 @@
+package com.example.androidthings.myproject;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattServer;
+import android.bluetooth.BluetoothGattServerCallback;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseData;
+import android.bluetooth.le.AdvertiseSettings;
+import android.bluetooth.le.BluetoothLeAdvertiser;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.ParcelUuid;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.ViewGroup;
+
+/**
+ * Created by MichaelOudenhoven on 11/3/17.
+ */
+
+public class HomeActivity extends Activity {
+    private static final String TAG = HomeActivity.class.getSimpleName();
+
+    /* Bluetooth API */
+    private BluetoothManager mBluetoothManager;
+    private BluetoothGattServer mBluetoothGattServer;
+    private BluetoothLeAdvertiser mBluetoothLeAdvertiser;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        /* Screen initialization */
+        setContentView(R.layout.homelayout);
+        getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        Log.d(TAG, "Display height in pixels: "+ dm.heightPixels);
+        Log.d(TAG, "Display width in pixels: "+ dm.widthPixels);
+        Log.d(TAG, "Display density in dpi: "+ dm.densityDpi);
+
+
+
+        /* Set up bluetooth */
+        mBluetoothManager = (BluetoothManager) getSystemService(BLUETOOTH_SERVICE);
+        BluetoothAdapter bluetoothAdapter = mBluetoothManager.getAdapter();
+        // We can't continue without proper Bluetooth support
+        if (!checkBluetoothSupport(bluetoothAdapter)) {
+            finish();
+        }
+
+        // IDD: SET A CUSTOM DEVICE NAME - is iMX7 by default
+        // @see https://stackoverflow.com/questions/8377558/change-the-android-bluetooth-device-name
+        // No more than 8 characters or advertising will fail (" LE Advertise Failed: 1")
+        bluetoothAdapter.setName("Aurai Pico");
+
+        // Register for system Bluetooth events
+        IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        registerReceiver(mBluetoothReceiver, filter);
+        if (!bluetoothAdapter.isEnabled()) {
+            Log.d(TAG, "Bluetooth is currently disabled...enabling");
+            bluetoothAdapter.enable();
+        } else {
+            Log.d(TAG, "Bluetooth enabled...starting services");
+            startAdvertising();
+            startServer();
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        BluetoothAdapter bluetoothAdapter = mBluetoothManager.getAdapter();
+        if (bluetoothAdapter.isEnabled()) {
+            stopServer();
+            stopAdvertising();
+        }
+
+        unregisterReceiver(mBluetoothReceiver);
+    }
+
+    /**
+     * Verify the level of Bluetooth support provided by the hardware.
+     * @param bluetoothAdapter System {@link BluetoothAdapter}.
+     * @return true if Bluetooth is properly supported, false otherwise.
+     */
+    private boolean checkBluetoothSupport(BluetoothAdapter bluetoothAdapter) {
+
+        if (bluetoothAdapter == null) {
+            Log.w(TAG, "Bluetooth is not supported");
+            return false;
+        }
+
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Log.w(TAG, "Bluetooth LE is not supported");
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Listens for Bluetooth adapter events to enable/disable
+     * advertising and server functionality.
+     */
+    private BroadcastReceiver mBluetoothReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.STATE_OFF);
+
+            switch (state) {
+                case BluetoothAdapter.STATE_ON:
+                    startAdvertising();
+                    startServer();
+                    break;
+                case BluetoothAdapter.STATE_OFF:
+                    stopServer();
+                    stopAdvertising();
+                    break;
+                default:
+                    // Do nothing
+            }
+
+        }
+    };
+
+    /**
+     * Begin advertising over Bluetooth that this device is connectable
+     * and supports the Current Time Service.
+     */
+    private void startAdvertising() {
+        BluetoothAdapter bluetoothAdapter = mBluetoothManager.getAdapter();
+
+        mBluetoothLeAdvertiser = bluetoothAdapter.getBluetoothLeAdvertiser();
+
+        if (mBluetoothLeAdvertiser == null) {
+            Log.w(TAG, "Failed to create advertiser");
+            return;
+        }
+
+        AdvertiseSettings settings = new AdvertiseSettings.Builder()
+                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_BALANCED)
+                .setConnectable(true)
+                .setTimeout(0)
+                .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
+                .build();
+
+        AdvertiseData data = new AdvertiseData.Builder()
+                .setIncludeDeviceName(true)
+                .setIncludeTxPowerLevel(false)
+                .addServiceUuid(new ParcelUuid(MotorControllerBLEProfile.CUSTOM_SERVICE))
+                .build();
+
+        mBluetoothLeAdvertiser
+                .startAdvertising(settings, data, mAdvertiseCallback);
+    }
+
+    /**
+     * Stop Bluetooth advertisements.
+     */
+    private void stopAdvertising() {
+        if (mBluetoothLeAdvertiser == null) return;
+
+        mBluetoothLeAdvertiser.stopAdvertising(mAdvertiseCallback);
+    }
+
+    /**
+     * Initialize the GATT server instance with the services/characteristics
+     * from the Time Profile.
+     */
+    private void startServer() {
+        mBluetoothGattServer = mBluetoothManager.openGattServer(this, mGattServerCallback);
+        if (mBluetoothGattServer == null) {
+            Log.w(TAG, "Unable to create GATT server");
+            return;
+        }
+
+
+        mBluetoothGattServer.addService(MotorControllerBLEProfile.createCustomService());
+
+
+    }
+
+    /**
+     * Shut down the GATT server.
+     */
+    private void stopServer() {
+        if (mBluetoothGattServer == null) return;
+
+        mBluetoothGattServer.close();
+    }
+
+    /**
+     * Callback to receive information about the advertisement process.
+     */
+    private AdvertiseCallback mAdvertiseCallback = new AdvertiseCallback() {
+        @Override
+        public void onStartSuccess(AdvertiseSettings settingsInEffect) {
+            Log.i(TAG, "LE Advertise Started.");
+        }
+
+        @Override
+        public void onStartFailure(int errorCode) {
+            Log.w(TAG, "LE Advertise Failed: "+errorCode);
+        }
+    };
+
+
+
+    /**
+     * Callback to handle incoming requests to the GATT server.
+     * All read/write requests for characteristics are handled here.
+     */
+    private BluetoothGattServerCallback mGattServerCallback = new BluetoothGattServerCallback() {
+
+        @Override
+        public void onConnectionStateChange(BluetoothDevice device, int status, int newState) {
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                Log.i(TAG, "BluetoothDevice CONNECTED: " + device);
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                Log.i(TAG, "BluetoothDevice DISCONNECTED: " + device);
+            }
+        }
+
+        /**
+         * Check for all writable characteristics
+         **/
+        @Override
+        public void onCharacteristicWriteRequest(BluetoothDevice device, int requestId, BluetoothGattCharacteristic characteristic, boolean preparedWrite, boolean responseNeeded, int offset, byte[] value) {
+
+            if (MotorControllerBLEProfile.OPEN_CLOSE.equals(characteristic.getUuid())) {
+                Log.i(TAG, "Write Output Characteristic");
+
+                MotorControllerBLEProfile.setWindowDirection(value);
+
+                if (responseNeeded) {
+                    mBluetoothGattServer.sendResponse(device,
+                            requestId,
+                            BluetoothGatt.GATT_SUCCESS,
+                            0,
+                            value);
+                }
+
+            }
+
+            else if (MotorControllerBLEProfile.SEND_POSITION.equals(characteristic.getUuid())) {
+                Log.i(TAG, "Write Output Characteristic");
+
+
+                MotorControllerBLEProfile.setWindowPos(value);
+
+                if (responseNeeded) {
+                    mBluetoothGattServer.sendResponse(device,
+                            requestId,
+                            BluetoothGatt.GATT_SUCCESS,
+                            0,
+                            value);
+                }
+            }
+
+        }
+
+        /**
+         * Check for all readable characteristics
+         **/
+        @Override
+        public void onCharacteristicReadRequest(BluetoothDevice device, int requestId, int offset,
+                                                BluetoothGattCharacteristic characteristic) {
+
+            if (MotorControllerBLEProfile.POSITION_FEEDBACK.equals(characteristic.getUuid())) {
+                Log.i(TAG, "Read Input Characteristic");
+//                mBluetoothGattServer.sendResponse(device,
+//                        requestId,
+//                        BluetoothGatt.GATT_SUCCESS,
+//                        0,
+//                        MotorControllerBLEProfile.getInputValue());
+            }
+
+            else {
+                // Invalid characteristic
+                Log.w(TAG, "Invalid Characteristic Read: " + characteristic.getUuid());
+                mBluetoothGattServer.sendResponse(device,
+                        requestId,
+                        BluetoothGatt.GATT_FAILURE,
+                        0,
+                        null);
+            }
+        }
+    };
+}
