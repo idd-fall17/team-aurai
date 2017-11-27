fba40139af8b4b5e9ac3885fd52adcb22097fff2
AshisGhosh <ashisghosh@live.com>
2017-11-27 11:09:26 -0800

    open to 100

--- picoproapp/src/main/java/com/example/androidthings/aurai/Constants.java
+++ picoproapp/src/main/java/com/example/androidthings/aurai/Constants.java
@@ -32,7 +32,7 @@ public class Constants {
 
 
     /*Positions for window proto 2 */
-    public static int open = 1;
+    public static int open = 100;
     public static int closed = 0;
     public static int window_setpoint;
     public static int window_position;
